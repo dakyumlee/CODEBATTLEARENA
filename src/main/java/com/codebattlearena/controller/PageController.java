ea9c9311def76384df7c12a7908761387100f46c
dakyumlee <oicrcutie@gmail.com>
2025-09-26 20:48:18 +0900

    Fix: Remove HomeController to resolve URL mapping conflict with PageController

--- src/main/java/com/codebattlearena/controller/PageController.java
+++ src/main/java/com/codebattlearena/controller/PageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
     @GetMapping("/")
-    public String index() {
+    public String home() {
         return "index";
     }
 
