49c1cd9502011c087d834b5e6b79f24b0c61aedf
dakyumlee <oicrcutie@gmail.com>
2025-09-28 20:24:54 +0900

    fix:경로변경

--- src/main/java/com/codebattlearena/controller/BattleController.java
+++ src/main/java/com/codebattlearena/controller/BattleController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @Controller
-@RequestMapping("/student/battle")
+@RequestMapping("/student/battles")
 public class BattleController {
 
     @GetMapping("")
